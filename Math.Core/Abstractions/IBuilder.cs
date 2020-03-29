3866abcb085ec3503a03debf96b85748b3b041d6
Mateo Mađerić <mateo.maderic@gmail.com>
2020-03-30 00:12:20 +0200

    builder abstractions

---
+++ Math.Core/Abstractions/IBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Math.Core.Enumerations;
+using System;
+
+namespace Math.Core.Abstractions
+{
+    public interface IBuilder
+    {
+        public Action<BuilderCommand, char?> Commander { get; set; }
+
+        public void UseDefaultCommander();
+        public void ExecuteCommand(BuilderCommand command, char? character = null);
+    }
+}
---
+++ Math.Core/Abstractions/INumberFactory.cs
@@ -0,0 +1,9 @@
+﻿using Math.Core.Literals;
+
+namespace Math.Core.Abstractions
+{
+    public interface INumberFactory
+    {
+        public Number Construct();
+    }
+}
