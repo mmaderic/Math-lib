6683143ec4b5a3139fd4d54f6ef6f1185a496391
Mateo Mađerić <mateo.maderic@gmail.com>
2020-04-04 20:17:57 +0200

    moved to builders namespace

---
+++ Math.Core/Builders/Calculation.cs
@@ -0,0 +1,51 @@
+﻿using Math.Core.Enumerations;
+using Math.Core.Extensions;
+using Math.Core.Literals;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Math.Core.Builders
+{
+    internal class Calculation : IComparable<Calculation>
+    {
+        public Number Left { get; }
+        public Number Right { get; }
+        public Operator Operator { get; }
+
+        public Calculation(Number left, Operator @operator, Number right)
+        {
+            Operator = @operator;
+            Left = left;
+            Right = right;
+        }        
+
+        public int CompareTo([AllowNull] Calculation other)
+        {
+            if (Operator.IsComplementary(other.Operator))
+                return 0;
+
+            if ((Operator == Operator.Addition || Operator == Operator.Subtraction) &&
+                other.Operator == Operator.Multiplication || other.Operator == Operator.Division)
+                return 1;
+
+            return -1;
+        }
+
+        public Number Calculate()
+        {
+            return Operator switch
+            {
+                Operator.Addition => Left + Right,
+                Operator.Subtraction => Left - Right,
+                Operator.Multiplication => Left * Right,
+                Operator.Division => Left / Right,
+                Operator.None when Right is null => Left,
+
+                _ => throw new NotImplementedException("Operator not implemented."),
+            };
+        }  
+
+        public override string ToString()
+            => $"{Left} {Operator.Sign()} {Right}";
+    }
+}
